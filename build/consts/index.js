cdd817e61acabcf89bddc89335a50ade0f2e252d
Robert Munteanu <robert.munteanu97@gmail.com>
2020-10-25 22:00:19 +0200

    Added build to ignored directories

--- build/consts/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STATUS_COMMAND = void 0;
-var STATUS_COMMAND = '!mcstatus';
-exports.STATUS_COMMAND = STATUS_COMMAND;
